98eb817647cf9ede6c4dda1f19fa0f2bc2f40047
Jason Short <jason@sheersky.com>
2014-08-21 20:44:54 -0700

    MoveTo - added automatic renaming Will now rename file automatically if the destination file already exists. Appends _NNN, where NNN starts at 001

--- smartfilesorter/actionrules/moveto.py
+++ smartfilesorter/actionrules/moveto.py
@@ -17,19 +17,33 @@ class MoveTo(ActionRule):
     def action(self, target, dry_run=False):
         """
         :param target: Full path and filename
-        :param dry_run: True - don't actually perform action. False: perform action. No effect for this rule.
+        :param dry_run: True - don't actually perform action. False: perform action.
         :return: filename: Full path and filename after action completes
         """
-        new_filename = target
+        # Get full path to the file in the destination directory
+        new_filename = os.path.join(self.destination, os.path.basename(target))
+
+        # If the file exists in the destination directory, append _NNN to the name where NNN is
+        # a zero padded number. Starts at _001
+        while os.path.exists(new_filename):
+            # if filename ends in _NNN, start there
+            filename, extension = os.path.splitext(os.path.basename(new_filename))
+            if filename[-4] == '_' and filename[-3:].isdigit():
+                current = int(filename[-3:]) + 1
+                new_filename = os.path.join(self.destination,
+                                            (filename[:-4] +
+                                             "_{0:03d}".format(current) +
+                                             extension))
+            else:
+                new_filename = os.path.join(self.destination,
+                                            (filename + "_001" + extension))
 
         if dry_run is False:
-            base_name = os.path.basename(target)
-            new_filename = os.path.join(self.destination, base_name)
             self.logger.debug("Moving {0} to {1}".format(target, new_filename))
 
             if not os.path.exists(new_filename):
                 try:
-                    shutil.move(target, self.destination)
+                    shutil.move(target, new_filename)
                 except IOError:
                     self.logger.error("Error moving file {0} to {1}".format(target, self.destination))
                     raise IOError
@@ -37,5 +51,8 @@ class MoveTo(ActionRule):
                 self.logger.error("Destination file already exists: {0}".format(new_filename))
                 raise IOError
 
+            return new_filename
 
-        return new_filename
+        else:
+            self.logger.debug("Dry run. Skipping move {0} to {1}".format(target, new_filename))
+            return target
--- tests/actionrules/test_moveto.py
+++ tests/actionrules/test_moveto.py
@@ -40,24 +40,40 @@ class TestMoveTo(unittest.TestCase):
         self.assertTrue(os.path.isfile(self.dest_file), "Destination file does not exist")
         self.assertFalse(os.path.isfile(self.source_file), "Source file still exists")
 
-    def test_moveto_dryrun(self):
+    def test_moveto_dry_run(self):
         # Test that file is NOT moved when dryrun=True
         new_filename = self.action.do_action(self.source_file, dry_run=True)
         self.assertEqual(new_filename, self.source_file)
         self.assertFalse(os.path.isfile(self.dest_file), "Destination file does not exist")
         self.assertTrue(os.path.isfile(self.source_file), "Source file still exists")
 
-    def test_moveto_dest_exists(self):
-        # Test that error is raised when destination file exists
+    def test_moveto_destination_exists(self):
+        # Test that if the file already exists in the destination directory, add _001
+        # to the filename and try again.
         shutil.copy(self.source_file, self.dest_dir)
-        self.assertRaises(IOError, self.action.do_action, self.source_file)
-        self.assertTrue(os.path.isfile(self.source_file), "Source file does not exist")
-        self.assertTrue(os.path.isfile(self.dest_file), "Destination file does not exist and should")
+        new_filename = self.action.do_action(self.source_file)
+        self.assertFalse(os.path.isfile(self.source_file), "Source file exists and should have been moved")
+        self.assertTrue(os.path.isfile(new_filename), "Destination file does not exist and should")
+        self.assertTrue(new_filename[-8:-4] == '_001', 'New file does not end in "_001"')
+
+    def test_moveto_destination_exists_twice(self):
+        # Test that if the file already exists in the destination directory, and a file with the _001 suffix
+        # in the filename exists, that the file will be renamed to <filename>_002.extension
+        shutil.copy(self.source_file, self.dest_dir)
+        filename, extension = os.path.splitext(self.dest_file)
+        file1 = os.path.join(self.dest_dir, (filename + '_001' + extension))
+        shutil.copy(self.source_file, file1)
+
+        new_filename = self.action.do_action(self.source_file)
+        self.assertFalse(os.path.isfile(self.source_file), "Source file exists and should have been moved")
+        self.assertTrue(os.path.isfile(new_filename), "Destination file does not exist and should")
+        self.assertTrue(new_filename[-8:-4] == '_002', 'New file does not end in "_002"')
+
+
 
     def tearDown(self):
         # Clean up temp files
         shutil.rmtree(self.source_dir)
-
 
 
 if __name__ == '__main__':
